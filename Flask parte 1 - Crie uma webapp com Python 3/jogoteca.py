45475ce8dd20e9c79d5f801639e573322382b1f8
Felipe Lovato <felipe.lovato95@gmail.com>
2022-02-23 20:48:36 -0300

    add

--- Flask parte 1 - Crie uma webapp com Python 3/jogoteca.py
+++ Flask parte 1 - Crie uma webapp com Python 3/jogoteca.py
@@ -2,9 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+class Jogos:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
 @app.route("/inicio")
 def ola():
-    lista_cursos = ["css", "java", "html", "python"]
-    lista_jogos = ['Tibia', 'CS', 'Lol', 'Combats']
-    return render_template("lista.html", titulo="JOGOS", jogos=lista_jogos, cursos=lista_cursos)
+    jogo1 = Jogos('tibia', 'rpg', 'pc')
+    jogo2 = Jogos('CS', 'tiro', 'Playstation')
+    lista = [jogo1,jogo2]
+    return render_template("lista.html", titulo='Jogos',Jogos=lista)
 app.run()
